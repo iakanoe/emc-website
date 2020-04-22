0e7edb599d425d63ad1dbb63b2f3cf41659a4ddb
iakanoe <iakanoe1056@gmail.com>
2020-04-22 03:16:59 -0300

    arreglo tema server 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const sass_middleware = require('node-sass-middleware');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = /* process.env.PORT || */ 8080;
 
 app.use(sass_middleware({
     src: __dirname + "/src",
